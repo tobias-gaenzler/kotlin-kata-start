16af19eb42e660e5a68516f9633448c7eec648b8
tobias-gaenzler <tobias-gaenzler@users.noreply.github.com>
2021-05-08 20:41:40 +0200

    Initial project setup

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val jUnitVersion = "5.7.1"
+val mockkVersion = "1.11.+"
+val koTestVersion = "4.5.+"
+
+plugins {
+    kotlin("jvm") version "1.5.0"
+}
+
+group = "de.tobiasgaenzler"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
+    testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("io.kotest:kotest-runner-junit5-jvm:$koTestVersion")
+    testImplementation("io.kotest:kotest-assertions-core-jvm:$koTestVersion")
+    testImplementation("io.kotest:kotest-property-jvm:$koTestVersion")
+    testImplementation("io.kotest:kotest-assertions-json:$koTestVersion")
+    testImplementation(kotlin("test-junit5"))
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.jvmTarget = "15"
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "kotlin-kata-start"
+
